255779ddbdbc597b26cfff60849191b0e9f30bc6
DEEV2007 <73585822+DEEV2007@users.noreply.github.com>
2021-04-08 16:02:35 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,20 @@
+class Block{
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options)
+        this.width=width
+        this.height=height
+
+        World.add(world,this.body)
+    }
+    display(){
+        var pose=this.body.position
+        push();
+        translate(pose.x,pose.y)
+        rectMode(CENTER)
+        rect(0,0,this.width,this.height)
+        pop();
+    }
+}
